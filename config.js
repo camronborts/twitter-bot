f4bfc6b1cd1d1940872a42c44d84d3024e9ca197
Camron Borts <37637004+camronborts@users.noreply.github.com>
2022-11-01 12:00:56 -0700

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    consumer_key:         'W1UhWwRYsqQY4IDyWhxYNJeAk',
-    consumer_secret:      'VLpuLLOU7zmQWKqHKEt4V87OJvWlErKTe3n567e92VtCq60iiO',
-    access_token:         '1587304056795844608-rtZSGhd1wjDfX509zCqfYJuOEW3qXg',
-    access_token_secret:  '9UDGHOba6OEytv78EJwkncPgzmzLI8LbmkRrEXbxBpMf9',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL:            true,     // optional - requires SSL certificates to be valid.
-    
-}
